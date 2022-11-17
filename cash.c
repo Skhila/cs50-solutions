09ab67ca652f4b29fa7c63e11125a32b860f6a78
Skhila <97901103+Skhila@users.noreply.github.com>
2022-11-17 19:47:07 +0400

    Create cash.c

---
+++ cash.c
@@ -0,0 +1,78 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Change Owed: ");
+
+    }
+    while (cents < 0);
+
+    return cents;
+}
+
+int calculate_quarters(int cents)
+{
+    //calculate quarters owed
+    int quarters_owed = floor(cents / 25);
+    return quarters_owed;
+}
+
+int calculate_dimes(int cents)
+{
+    //calculate dimes owed
+    int dimes_owed = floor(cents / 10);
+    return dimes_owed;
+}
+
+int calculate_nickels(int cents)
+{
+    //calculate nickels owed
+    int nickels_owed = floor(cents / 5);
+    return nickels_owed;
+}
+
+int calculate_pennies(int cents)
+{
+    //calculate pennies owed
+    int pennies_owed = floor(cents / 1);
+    return pennies_owed;
+}
